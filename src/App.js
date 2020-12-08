380a05c915685494ee154ef63c57077431aa1eca
Jeffrey Lei <jlei9099@bths.edu>
2020-12-07 19:48:21 -0500

    src files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's Build a Discord Clone!!!</h2>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
