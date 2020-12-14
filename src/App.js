7bf44be10011e5e1bf896103d2ebe2dcf025bd44
Raymond Luy <rluy.97@gmail.com>
2020-12-13 19:56:58 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './App.css';
 import Sidebar from "./Sidebar";
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
 function App() {
   return (
     <div className="App">
       <h2>Let's Build a Discord Clone!!!</h2>
       <Sidebar />
+      <Login />
+      <SignUp />
     </div>
   );
 }
+
 
 export default App;
