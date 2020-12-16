2a48061845cb674e33435868f9e9dc5d68c6194c
Jeffrey Lei <jlei000@ccny.cuny.edu>
2020-12-15 22:24:57 -0500

    Big changes
    Fixed client side to use flask instead of express

---
+++ server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from models import db
+
+app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'Accounts',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
