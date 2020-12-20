eeeb04bd2c4086b9dd94c1a6f4e92b70b371e701
Raymond Luy <rluy.97@gmail.com>
2020-12-20 11:12:20 -0500

    Added

---
+++ client/src/components/Chat.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`);
+
+const Chat = () => {
+
+  useEffect(() => {
+    getMessages();
+  }, [messages.length]);
+
+  const getMessages = () => {
+    socket.on("message", msg => {
+      setMessages([...messages, msg]);
+    });
+  };
+
+  const onChange = e => {
+    setMessage(e.target.value);
+  };
+
+  const onClick = () => {
+    if (message !== "") {
+      socket.emit("message", message);
+      setMessage("");
+    } else {
+      alert("Please Add A Message");
+    }
+  };
+
+  return (
+    <div>
+      {messages.length > 0 &&
+        messages.map(msg => (
+          <div>
+            <p>{msg}</p>
+          </div>
+        ))}
+      <input value={message} name="message" onChange={e => onChange(e)} />
+      <button onClick={() => onClick()}>Send</button>
+    </div>
+  );
+};
+
+export default Chat;
